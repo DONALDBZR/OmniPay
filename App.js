961df76d5542732fb5bbad024e0e35966ed0723f
DONALDBZR <andygaspard@hotmail.com>
2022-06-14 22:25:58 +0400

    Update 2
    Debugging

--- App.js
+++ App.js
@@ -4,30 +4,26 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 // App class
 class App {
-	// Constructor method
-	constructor() {
-		// App.render()
-		this.render();
-	}
-	// Render method
-	render() {
-		console.log("Application executed!");
-		return (
-			<View style={styles.container}>
-				<Text>Open up App.js to start working on your application!</Text>
-				<StatusBar style="auto" />
-			</View>
-		);
-	}
+    // Constructor method
+    constructor() { }
+    // Render method
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+                <StatusBar style="auto" />
+            </View>
+        );
+    }
 }
 // Creating the stylesheet
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: 'rgb(255, 0, 0)',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+    container: {
+        flex: 1,
+        backgroundColor: 'rgb(255, 0, 0)',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
 // Exporting components
-export { App }
+export { App };
---
+++ Driver.js
@@ -0,0 +1,10 @@
+// Importing the application
+import { App } from "./App";
+// Instantiating the application
+let application = new App();
+// Application function
+function Application() {
+	return application.render();
+}
+// Exporting components
+export { Application };
